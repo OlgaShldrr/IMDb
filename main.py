5f2dd5f26a28c08d436d97068bbebe96884f318b
OlgaShldrr <sholderer@gmail.com>
2020-10-30 16:35:11 +0100

    changes with the instructor

--- main.py
+++ main.py
@@ -10,6 +10,44 @@ from preferences import Preferences
  #   somelist = [name,age,genre,language,year,recommendation,rent]
  #   writer.writerow((somelist))
 
+def get_recommendation():
+    recommendation = []
+    if genre and language and year:
+        recommendation = imdb_df[(imdb_df['genre'].str.contains(genre)) & (imdb_df['language'].str.contains(language)) & (imdb_df['year']==year)].sort_values(by='reviews_from_critics', ascending=False).head(1)[
+            'original_title']
+
+    elif genre and language:
+        recommendation = imdb_df[
+            (imdb_df['genre'].str.contains(genre)) & (imdb_df['language'].str.contains(language))].sort_values(by='reviews_from_critics', ascending=False).head(1)[
+            'original_title']
+
+    elif genre and year:
+        recommendation = imdb_df[
+            (imdb_df['genre'].str.contains(genre)) & (
+                        imdb_df['year'] == year)].sort_values(by='reviews_from_critics', ascending=False).head(1)[
+            'original_title']
+
+    elif language and year:
+        recommendation = imdb_df[(imdb_df['language'].str.contains(language)) &
+            ( imdb_df['year'] == year)].sort_values(by='reviews_from_critics', ascending=False).head(1)[
+            'original_title']
+
+    elif language:
+        recommendation = imdb_df[imdb_df['language'].str.contains(language)].sort_values(by='reviews_from_critics',
+                                                                        ascending=False).head(1)[
+            'original_title']
+
+    elif year:
+        recommendation = imdb_df[imdb_df['year'].str.contains(year)].sort_values(by='reviews_from_critics',
+                                                                        ascending=False).head(1)[
+            'original_title']
+
+    elif genre:
+        recommendation = imdb_df[imdb_df['genre'].str.contains(genre)].sort_values(by='reviews_from_critics',
+                                                                        ascending=False).head(1)[
+            'original_title']
+    return recommendation
+
 def  rent_option():
     global rent
     questions = [
@@ -40,25 +78,17 @@ def get_preferences():
     genre = input("What genre would you like to watch?")
     language = input("What should be the original language?")
     preferences = Preferences(name, language, year, genre)
+    return preferences
 
 
 def main():
-    global recommendation
+
     print("Movie recommendation program")
     get_know_user()
     print("Let's figure out your movie preferences")
     get_preferences()
-
-
-
-    try:
-        recommendation = imdb_df[(imdb_df['genre'].str.contains(genre)) & (imdb_df['language'].str.contains(language)) & (imdb_df['year']==year)].sort_values(by='reviews_from_critics', ascending=False).head(1)[
-            'original_title']
-        print("Here is our recommendation: ", recommendation)
-    except Exception as e:
-        ("No matching movies :( Try different genre?")
-        print(e)
-
+    recommendation = get_recommendation()
+    print("Here is our recommendation: ", recommendation)
     rent_option()
     #get_log()
 
