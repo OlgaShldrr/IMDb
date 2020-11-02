343767452b12affa80e4acc9184774ef3618c7c9
jingzi_xu <jingzxu@deloitte.nl>
2020-11-02 13:34:04 +0100

    modify user.py and welcome message part in main.py

--- User.py
+++ User.py
@@ -3,7 +3,5 @@ class User:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-        if int(age)>=18:
-            print("Welcome ", name, "! Thanks for providing your age (", age,"). You are above the legal age, so your movie recommendation will be adjusted accordingly")
-        else:
-            print("Welcome ", name, "! Thanks for providing your age (", age, "). You are below the legal age, so your movie recommendation will be adjusted accordingly")
+
+
--- main.py
+++ main.py
@@ -3,16 +3,33 @@ imdb_df = pd.read_csv('data\IMDb movies.csv', low_memory=False)
 from user import User
 from preferences import Preferences
 
+#ask user name and age
 def get_know_user():
-    global name
-    name = input('What is your name?')
-    age = input('What is your age?')
+    #while True:
+    user_name_input = input('What is your name?')
+    name = user_name_input.strip() # remove spaces
+    if name:
+        name = name.capitalize() # name starts with capital
+    age = int(input('What is your age?')) #age is integer
+
     user = User(name, age)
+    return user
+
+#age verification
+def age_verification(user):
+    if int(user.age)>=18:
+        print("Welcome ", user.name, "! Thanks for providing your age (",
+                  user.age,"). You are above the legal age, so your movie recommendation will be adjusted accordingly")
+    else:
+        print("Welcome ", user.name, "! Thanks for providing your age (",
+                  user.age, "). You are below the legal age, so your movie recommendation will be adjusted accordingly")
 
 
 def main():
     print("Movie recommendation program")
-    get_know_user()
+    user= get_know_user()
+    age_verification(user)
+
     print("Let's figure out your movie preferences")
     preference = Preferences(name)
     #language = Preferences.get_language()
